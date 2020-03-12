86f6ceb09754d844dae02eb5d61fc6c4db27f1f6
WildanYR <wildanyahya99@gmail.com>
2020-03-12 13:21:27 +0700

    tambahkan icon fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ module.exports = {
   ** Global CSS
   */
   css: [
+    '@fortawesome/fontawesome-free/css/all.css'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -56,19 +57,8 @@ module.exports = {
   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+    icons: {
+      iconfont: 'fa'
     }
   },
   /*
